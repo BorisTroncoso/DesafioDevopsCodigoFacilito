948313c3d0a031dc81db800e63b4a78a441ca6fe
BorisTroncoso <150861586+BorisTroncoso@users.noreply.github.com>
2024-08-29 00:50:19 -0400

    Update Hours.js

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,30 @@
-// Placeholder to host a component
+import React from 'react';
+
+const Hours = () => {
+  const getCurrentHours = () => {
+    const today = new Date();
+    const dayOfWeek = today.getDay(); // 0 (Domingo) - 6 (Sábado)
+    let hours;
+
+    switch (dayOfWeek) {
+      case 0: // Domingo
+      case 6: // Sábado
+        hours = '9:00 AM - 8:00 PM';
+        break;
+      default: // Lunes a Viernes
+        hours = '10:00 AM - 4:00 PM';
+        break;
+    }
+
+    return hours;
+  };
+
+  return (
+    <div>
+      <h3>Horario de hoy:</h3>
+      <p>{getCurrentHours()}</p>
+    </div>
+  );
+};
+
+export default Hours;// Placeholder to host a component
